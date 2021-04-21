c1faba749515f0b34f89b881ba38725a9a347d2c
Azaran <karol.chanaj3@gmail.com>
2021-04-21 11:36:01 +0200

    implemented commands /exit /kick /ban

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,6 +1,7 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -9,6 +10,8 @@ import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.nio.channels.Channel;
+import java.util.Locale;
 import java.util.Objects;
 
 class JDAService {
@@ -80,12 +83,93 @@ public class Bot extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
-        window.printText(event.getMember().getEffectiveName() + ": " + event.getMessage().getContentRaw());
+        Message msg = event.getMessage();
+        Member member = event.getMember();
+        String messageContent = msg.getContentRaw();
+        MessageChannel channel = event.getChannel();
+
+        String[] messageContentParams = messageContent.split("\\s+");
+
+        window.printText(event.getMember().getEffectiveName() + ": " + messageContent);
+
+        if (messageContent.toLowerCase().startsWith("/kick")){
+            if (msg.getMentionedMembers().isEmpty()){
+                return;
+            }
+            if(messageContentParams.length < 3) {
+                channel.sendMessage("Missing reason!").queue();
+                return;
+            }
+
+            StringBuilder reason = new StringBuilder();
+            for(int i = 2; i < messageContentParams.length; i++){
+                reason.append(messageContentParams[i]).append(" ");
+            }
+            channel.sendMessage("Reason: " + reason).queue();
+
+            Member target = msg.getMentionedMembers().get(0);
+            if (!member.canInteract(target) || !member.hasPermission(Permission.KICK_MEMBERS)) {
+                channel.sendMessage("You need permission to kick!").queue();
+                return;
+            }
+
+            final Member selfMember = event.getGuild().getSelfMember();
+
+            if (!selfMember.canInteract(target) || !selfMember.hasPermission(Permission.KICK_MEMBERS)) {
+                event.getChannel().sendMessage("I don't have permission to kick!").queue();
+                return;
+            }
+
+
+            event.getGuild()
+                    .kick(target, reason.toString())
+                    .reason(reason.toString())
+                    .queue(
+                            (__) -> event.getChannel().sendMessage("Kick was successful").queue(),
+                            (error) -> event.getChannel().sendMessageFormat("Could not kick %s", error.getMessage()).queue()
+                    );
+        }
+        if (messageContent.toLowerCase().startsWith("/ban")){
+            if (msg.getMentionedMembers().isEmpty()){
+                return;
+            }
+            if(messageContentParams.length < 3) {
+                channel.sendMessage("Missing reason!").queue();
+                return;
+            }
+
+            StringBuilder reason = new StringBuilder();
+            for(int i = 2; i < messageContentParams.length; i++){
+                reason.append(messageContentParams[i]).append(" ");
+            }
+
+            Member target = msg.getMentionedMembers().get(0);
+            if (!member.canInteract(target) || !member.hasPermission(Permission.BAN_MEMBERS)) {
+                channel.sendMessage("You need permission to ban members!").queue();
+                return;
+            }
+
+            final Member selfMember = event.getGuild().getSelfMember();
+
+            if (!selfMember.canInteract(target) || !selfMember.hasPermission(Permission.BAN_MEMBERS)) {
+                event.getChannel().sendMessage("I don't have permission to ban members!").queue();
+                return;
+            }
+
+
+            event.getGuild()
+                    .ban(target,0, reason.toString())
+                    .reason(reason.toString())
+                    .queue(
+                            (__) -> event.getChannel().sendMessage("Ban was successful").queue(),
+                            (error) -> event.getChannel().sendMessageFormat("Could not ban %s", error.getMessage()).queue()
+                    );
+        }
     }
 
     private void sendMessage() {
         String messageToSend = window.messageTextField.getText();
-        if(messageToSend.equals("exit")){
+        if(messageToSend.equals("/exit")){
             jda.disconnect();
         }
         window.messageTextField.setText("");
